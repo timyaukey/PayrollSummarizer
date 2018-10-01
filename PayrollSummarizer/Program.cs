651c31038e0cccef13c4015e7acf923eb0e714e2
Timothy Yaukey <yaukey@willowsoft.com>
2018-09-30 18:45:30 -0700

    Add GitHub repo.

--- PayrollSummarizer/Program.cs
+++ PayrollSummarizer/Program.cs
@@ -15,7 +15,7 @@ namespace PayrollSummarizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
